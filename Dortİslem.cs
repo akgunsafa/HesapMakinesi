c208d2ab500a33d762b2a725003f6a4feedd2bc2
Safa Akgün <safa.akgun@hotmail.com>
2021-01-17 21:05:53 +0300

    Add project files.

---
+++ Dortİslem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HesapMakinesi
+{
+    class Dortİslem
+    {
+        public void Topla(double sayi1, double sayi2)
+        {
+            double sonuc = sayi1 + sayi2 ;
+            Console.WriteLine(sayi1+ " + " + sayi2 + " = " + sonuc);
+        }
+        public void Cikar(double sayi1, double sayi2)
+        {
+            double sonuc = sayi1 - sayi2 ;
+            Console.WriteLine(sayi1 + " - " + sayi2 + " = " + sonuc);
+        }
+        public void Carp(double sayi1, double sayi2)
+        {
+            double sonuc = sayi1 * sayi2 ;
+            Console.WriteLine(sayi1 + " * " + sayi2 + " = " + sonuc);
+        }
+        public void Böl(int sayi1, int sayi2)
+        {
+            int sonuc =sayi1 / sayi2 ;
+            int mod = sayi1 % sayi2;
+            if (mod==0)
+            {
+                Console.WriteLine(sayi1 + " / " + sayi2 + " = " + sonuc);
+            }
+            else
+            {
+                Console.WriteLine(sayi1 + " / " + sayi2 + " = " + sonuc +" || " + "kalan: "+mod);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dortİslem dortİslem = new Dortİslem();
+            Console.WriteLine("yapmak istediğiniz işlemi giriniz(toplama/çıkarma/çarpma/bölme):  ");
+            string islem = Console.ReadLine();
+           
+            Console.WriteLine("ilk sayıyı giriniz : ");
+            int sayi1 = Int32.Parse(Console.ReadLine());
+           
+            Console.WriteLine("ikinci sayıyı giriniz : ");
+            int sayi2 = Int32.Parse(Console.ReadLine());
+
+            switch (islem)
+            {
+                case "toplama":
+                    dortİslem.Topla(sayi1 ,sayi2);
+                    break;
+                case "çıkarma":
+                    dortİslem.Cikar(sayi1 ,sayi2);
+                    break;
+                case "çarpma":
+                    dortİslem.Carp(sayi1 ,sayi2);
+                    break;
+                case "bölme":
+                    dortİslem.Böl(sayi1 ,sayi2);
+                    break;
+                default:
+                    Console.WriteLine("işlem ismi yanlış yazıldı!!");
+                    break;
+
+            }
+        }
+    }
+}
